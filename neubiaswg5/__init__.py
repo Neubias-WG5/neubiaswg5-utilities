3d04f96f4a21b08432b3a90b45ec73ce67c43d2a
Waliens <romainmormont@hotmail.com>
2018-11-19 11:19:03 +0100

    add variables for problem classes

--- neubiaswg5/__init__.py
+++ neubiaswg5/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
 __version__ = "0.1.0"
+
+from .problemclass import *
+
+__all__ = ["CLASS_OBJSEG", "CLASS_SPTCNT", "CLASS_PIXCLA", "CLASS_TRETRC", "CLASS_LOOTRC", "CLASS_OBJDET",
+           "CLASS_PRTTRK", "CLASS_OBJTRK"]
---
+++ neubiaswg5/problemclass.py
@@ -0,0 +1,12 @@
+
+"""
+Neubias problem classes
+"""
+CLASS_OBJSEG = "ObjSeg"
+CLASS_SPTCNT = "SptCnt"
+CLASS_PIXCLA = "PixCla"
+CLASS_TRETRC = "TreTrc"
+CLASS_LOOTRC = "LooTrc"
+CLASS_OBJDET = "ObjDet"
+CLASS_PRTTRK = "PrtTrk"
+CLASS_OBJTRK = "ObjTrk"
