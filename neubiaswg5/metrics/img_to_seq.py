21e99f1a5d83f1ce6fb60d48f859d7f3deb6e81a
SebastienTs <sebastien.tosi@gmail.com>
2018-12-18 11:39:47 +0100

    Read stack dimensions as arguments

--- neubiaswg5/metrics/img_to_seq.py
+++ neubiaswg5/metrics/img_to_seq.py
@@ -5,17 +5,12 @@ import tifffile as tiff
 import os
 
 # Convert the tracking results saved in an OME-TIFF image to a sequence of images
-def img_to_seq(fname, out_dir, template):
-    # Should be adapted to also work with regular multi-tiff files
-    with tiff.TiffFile(fname) as img:
-        T = img.ome_metadata.get('Image').get('Pixels').get('SizeT')
-        Z = img.ome_metadata.get('Image').get('Pixels').get('SizeZ')
-        Y = img.ome_metadata.get('Image').get('Pixels').get('SizeY')
-        X = img.ome_metadata.get('Image').get('Pixels').get('SizeX')
-        img_data = img.asarray().ravel()
-        index = 0
-        offset = Z*Y*X
-        for t in range(T):
-            with tiff.TiffWriter(os.path.join(out_dir, template + '{0:03d}.tif'.format(t))) as frame:
-                frame.save(img_data[index:index+offset].reshape((Z,Y,X)))
-            index += offset
+def img_to_seq(fname, out_dir, template,X,Y,Z,T):
+    img = tiff.TiffFile(fname)
+    img_data = img.asarray().ravel()
+    index = 0
+    offset = Z*Y*X
+    for t in range(T):
+        with tiff.TiffWriter(os.path.join(out_dir, template + '{0:03d}.tif'.format(t))) as frame:
+            frame.save(img_data[index:index+offset].reshape((Z,Y,X)))
+        index += offset
