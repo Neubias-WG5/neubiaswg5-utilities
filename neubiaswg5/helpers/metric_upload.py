c52e6dfc923146150f95961372e9adae2700ff61
Waliens <romainmormont@hotmail.com>
2019-01-15 14:16:07 +0100

    ongoing implementation of metric upload

--- neubiaswg5/helpers/metric_upload.py
+++ neubiaswg5/helpers/metric_upload.py
@@ -1,9 +1,10 @@
 import os
 
 from cytomine import CytomineJob
-from cytomine.models import Property
+from cytomine.models import Property, Project
 
 from neubiaswg5 import CLASS_OBJSEG
+from neubiaswg5.helpers.cytomine_metrics import MetricCollection, get_metric_result_collection, get_metric_result
 from neubiaswg5.metrics import computemetrics_batch
 
 
@@ -35,7 +36,7 @@ def upload_metrics(problemclass, nj, inputs, gt_path, out_path, tmp_path, metric
         return
     if metric_params is None:
         metric_params = dict()
-    additional_properties = dict() # properties to be added in addition to the metrics and their parameters
+    additional_properties = dict()  # properties to be added in addition to the metrics and their parameters
 
     # get image names
     filenames = ["{}.tif".format(image.id) if do_download else os.path.basename(image) for image in inputs]
@@ -46,6 +47,25 @@ def upload_metrics(problemclass, nj, inputs, gt_path, out_path, tmp_path, metric
     ])
     additional_properties["IMAGES"] = str([im.id if do_download else os.path.basename(im) for im in inputs])
     results = computemetrics_batch(outfiles, reffiles, problemclass, tmp_path, **metric_params)
+
+    # effectively upload metrics
+    project = Project().fetch(nj.project.id)
+    metrics = MetricCollection().fetch_with_filter("discipline", project.discipline)
+
+    metric_collection = get_metric_result_collection(inputs)
+
+
+    for metric_name, values in results.items():
+        metric = metrics.find_by_attribute("shortName", metric_name)
+        if metric is None:
+            nj.logger.info("Skip metric '{}' because not listed as a metric of the problem class '{}'.".format(metric_name, problemclass))
+            continue
+        for _input, metric_value in zip(inputs, values):
+            [ for ]
+            metric_collection.append(get_metric_result(_input, metric_id=metric.id, id_job=nj.job.id, value=metric_value,
+                                                       property=[]))
+
+    metric_collection.save()
 
     for key, value in results.items():
         Property(nj.job, key=key, value=str(value)).save()
