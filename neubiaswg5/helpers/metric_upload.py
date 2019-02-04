e037b1161fe114d8430d55c800aa539e8e0d26d1
Waliens <romainmormont@hotmail.com>
2019-02-04 11:55:31 +0100

    refactor input image format + extend support to other ground truth formats than tif

--- neubiaswg5/helpers/metric_upload.py
+++ neubiaswg5/helpers/metric_upload.py
@@ -9,7 +9,7 @@ from neubiaswg5.metrics import computemetrics_batch
 
 
 def upload_metrics(problemclass, nj, inputs, gt_path, out_path, tmp_path, metric_params=None,
-                   do_download=False, do_compute_metrics=False, **kwargs):
+                   do_compute_metrics=False, gt_inputs=None, **kwargs):
     """Upload each sample will get a value for each metrics of the given problemclass
     Parameters
     ----------
@@ -27,10 +27,11 @@ def upload_metrics(problemclass, nj, inputs, gt_path, out_path, tmp_path, metric
         Absolute path to a temporary folder
     metric_params: dict
         Additional parameters for metric computation (forwarded to computemetrics or computemetrics_batch directly)
-    do_download: bool
-        Whether or not the images were downloaded from the server.
     do_compute_metrics: bool
         Whether or not to compute and upload the metrics.
+    gt_inputs: list|None
+        If there is a mismatch between file format of input and ground truth, the ground truth inputs should be passed
+        through this parameter.
     """
     if not do_compute_metrics:
         return
@@ -38,12 +39,18 @@ def upload_metrics(problemclass, nj, inputs, gt_path, out_path, tmp_path, metric
         metric_params = dict()
 
     # get image names
-    filenames = ["{}.tif".format(image.id) if do_download else os.path.basename(image) for image in inputs]
     outfiles, reffiles = zip(*[
-        (os.path.join(out_path, filename),
-         os.path.join(gt_path, filename))
-        for filename in filenames
+        (os.path.join(out_path, in_image.filename),
+         os.path.join(gt_path, in_image.filename))
+        for in_image in (inputs if gt_inputs is None else gt_inputs)
     ])
+
+    # check that output files exist
+    for outfile, reffile in zip(outfiles, reffiles):
+        if not os.path.isfile(outfile):
+            raise FileNotFoundError("There should be one output file for each ground truth file."
+                                    "Output file '{}' for reference file '{}' does not exist.".format(outfile, reffiles))
+
     results, params = computemetrics_batch(outfiles, reffiles, problemclass, tmp_path, **metric_params)
 
     # effectively upload metrics
@@ -59,14 +66,15 @@ def upload_metrics(problemclass, nj, inputs, gt_path, out_path, tmp_path, metric
             nj.logger.info("Skip metric '{}' because not listed as a metric of the problem class '{}'.".format(metric_name, problemclass))
             continue
         # create metric results
-        for i, _input in enumerate(inputs):
-            metric_result = get_metric_result(_input, id_metric=metric.id, id_job=nj.job.id, value=values[i])
+        for i, in_image in enumerate(inputs):
+            image = in_image.object
+            metric_result = get_metric_result(image, id_metric=metric.id, id_job=nj.job.id, value=values[i])
             metric_collection.append(metric_result)
 
             # for logging
-            image_dict = per_input_metrics.get(_input.id, {})
+            image_dict = per_input_metrics.get(image.id, {})
             image_dict[metric.shortName] = image_dict.get(metric.shortName, []) + [values[i]]
-            per_input_metrics[_input.id] = image_dict
+            per_input_metrics[image.id] = image_dict
 
     nj.logger.log(logging.DEBUG, "Metrics:")
     for _name, _metrics in per_input_metrics.items():
