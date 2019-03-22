439f8366cf7a47fea7b510784a7b5e0ce441502f
Leandro Aluisio Scholz <leandro.a.scholz@gmail.com>
2019-03-22 10:00:43 +0100

    fixed call dor DiademMetric.jar in TreTrc problem.
    used call without '-D 0' in the end because it did not like (apparently). Default is -D 0 so no problem not to use that argument.

--- neubiaswg5/metrics/compute_metrics.py
+++ neubiaswg5/metrics/compute_metrics.py
@@ -158,13 +158,15 @@ def _computemetrics(infile, reffile, problemclass, tmpfolder, **extra_params):
 
         # call node_sorter functions to order swc and saves it with the same name and path
         swc_node_sorter(infile)
+        #print('Finished running .swc node sorter ...')
         # run diadem metric
-        command = "java -jar /usr/bin/DiademMetric.jar -G " + infile +" -T " + reffile + "-D 0"
+        command = "java -jar /usr/bin/DiademMetric.jar -G " + infile +" -T " + reffile
         run_metric = subprocess.run(command, shell=True, stdout = subprocess.PIPE)
         # Gets output result which looks like this 'b'Score: 0\n'
         # first splits by :, then splits by \\ to get the number
         # and finally removes any spaces.
         diadem = str(run_metric.stdout).split(':')[1].split('\\')[0].strip()
+        #print('Diadem metric score for '+ infile + ' is:' + str(diadem))
         metrics_dict["DM"] = float(diadem)
 
     elif problemclass == CLASS_LOOTRC:
