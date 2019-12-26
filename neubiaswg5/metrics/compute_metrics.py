adce5c9b67909fa9e957288dcf9881491c8964b0
Lassi Paavolainen <lassi.paavolainen@gmail.com>
2019-12-26 23:01:18 +0200

    Added metric mAP for object segmentation

--- neubiaswg5/metrics/compute_metrics.py
+++ neubiaswg5/metrics/compute_metrics.py
@@ -187,6 +187,10 @@ def _computemetrics(infile, reffile, problemclass, tmpfolder, **extra_params):
 
         metrics_dict["FOVL"] = float(fraction_overlap(image_gt, image_out))
 
+        dsb_metrics = pd.DataFrame(columns=["Image", "Threshold", "F1", "Jaccard", "TP", "FP", "FN", "Official_Score", "Precision", "Recall"])
+        dsb_metrics = compute_af1_results(image_gt, image_out, dsb_metrics, os.path.basename(infile))
+        metrics_dict["mAP"] = dsb_metrics['Official_Score'].mean()
+
     elif problemclass == CLASS_SPTCNT:
 
         Pred_ImFile = tiff.TiffFile(infile)
@@ -416,3 +420,74 @@ def _computemetrics(infile, reffile, problemclass, tmpfolder, **extra_params):
         metrics_dict.update({name: value for name, value in zip(metric_names, bchmetrics)})
 
     return metrics_dict, params_dict
+
+
+# Following methods have been copied from
+# https://github.com/carpenterlab/2019_caicedo_dsb/blob/master/evaluation.py
+def intersection_over_union(ground_truth, prediction):
+    # Count objects
+    true_objects = len(np.unique(ground_truth))
+    pred_objects = len(np.unique(prediction))
+    
+    # Compute intersection
+    h = np.histogram2d(ground_truth.flatten(), prediction.flatten(), bins=(true_objects,pred_objects))
+    intersection = h[0]
+    
+    # Area of objects
+    area_true = np.histogram(ground_truth, bins=true_objects)[0]
+    area_pred = np.histogram(prediction, bins=pred_objects)[0]
+    
+    # Calculate union
+    area_true = np.expand_dims(area_true, -1)
+    area_pred = np.expand_dims(area_pred, 0)
+    union = area_true + area_pred - intersection
+    
+    # Exclude background from the analysis
+    intersection = intersection[1:,1:]
+    union = union[1:,1:]
+    
+    # Compute Intersection over Union
+    union[union == 0] = 1e-9
+    IOU = intersection/union
+    
+    return IOU
+
+def measures_at(threshold, IOU):
+    matches = IOU > threshold
+    
+    true_positives = np.sum(matches, axis=1) == 1   # Correct objects
+    false_positives = np.sum(matches, axis=0) == 0  # Extra objects
+    false_negatives = np.sum(matches, axis=1) == 0  # Missed objects
+    
+    assert np.all(np.less_equal(true_positives, 1))
+    assert np.all(np.less_equal(false_positives, 1))
+    assert np.all(np.less_equal(false_negatives, 1))
+    
+    TP, FP, FN = np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
+    
+    f1 = 2*TP / (2*TP + FP + FN + 1e-9)
+    official_score = TP / (TP + FP + FN + 1e-9)
+
+    precision = TP / (TP + FP + 1e-9)
+    recall = TP / (TP + FN + 1e-9)
+    
+    return f1, TP, FP, FN, official_score, precision, recall
+
+# Compute Average Precision for all IoU thresholds
+def compute_af1_results(ground_truth, prediction, results, image_name):
+    # Compute IoU
+    IOU = intersection_over_union(ground_truth, prediction)
+    if IOU.shape[0] > 0:
+        jaccard = np.max(IOU, axis=0).mean()
+    else:
+        jaccard = 0.0
+    
+    # Calculate F1 score at all thresholds
+    for t in np.arange(0.5, 1.0, 0.05):
+        f1, tp, fp, fn, os, prec, rec = measures_at(t, IOU)
+        res = {"Image": image_name, "Threshold": t, "F1": f1, "Jaccard": jaccard, 
+               "TP": tp, "FP": fp, "FN": fn, "Official_Score": os, "Precision": prec, "Recall": rec}
+        row = len(results)
+        results.loc[row] = res
+        
+    return results
